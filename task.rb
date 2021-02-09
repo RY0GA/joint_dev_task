ef0dc23fb56b72c8c219bf16dd8d1c992315667f
RY0GA <ryoga.isota@gmail.com>
2021-02-09 10:30:38 +0900

    Q2,Q3の修正とQ8~Q11の回答を追加

--- task.rb
+++ task.rb
@@ -14,16 +14,15 @@ def q2
   array2 = ["bird", "bat", "tiger"]
 
   # 以下に回答を記載
-  array1 << array2
-  array1.flatten!
-  p array1
+  array3 = array1 + array2
+  p array3
 end
 
 def q3
   numbers = [1, 5, 8, 10, 2, 3, 2, 3, 3, 1, 4, 5, 9]
 
   # 以下に回答を記載
-  p numbers.count{ |number| number == 3}
+  p numbers.count(3)
 end
 
 def q4
@@ -66,7 +65,8 @@ def q8
   programming_languages = %w(ruby php python javascript)
 
   # 以下に回答を記載
-
+  programming_languages = %w(ruby php python javascript).map(&:capitalize)
+  upper_case_programming_languages = %w(ruby php python javascript).map(&:upcase)
   # 以下は変更しないで下さい
   p programming_languages
   p upper_case_programming_languages
@@ -76,21 +76,34 @@ def q9
   names = ["田中", "佐藤", "佐々木", "高橋"]
 
   # 以下に回答を記載
-
+  names.each.with_index(1) do |name, i|
+    puts "会員No.#{i} #{name}さん"
+  end
 end
 
 def q10
   foods = %w(いか たこ うに しゃけ うにぎり うに軍艦 うに丼)
 
   # 以下に回答を記載
-
+  foods.each do |food|
+    if food.include?("うに")
+      puts "好物です"
+    else
+      puts "まぁまぁ好きです"
+    end
+  end
 end
 
 def q11
   sports = ["サッカー", "バスケ", "野球", ["フットサル", "野球"], "水泳", "ハンドボール", ["卓球", "サッカー", "ボルダリング"]]
 
   # 以下に回答を記載
+  flatten_sports = sports.flatten
+  sports2 = flatten_sports.uniq
 
+  sports2.each.with_index(1) do |sport, i|
+    puts "No.#{i} #{sport}"
+  end
 end
 
 def q12
